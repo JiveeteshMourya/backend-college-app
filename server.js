86f4ca18314b2bd7c089524643cb9bc8af65ba75
Jiveetesh Mourya <jcodestudy@gmail.com>
2025-10-27 14:56:51 +0530

    Add CORS support, logging, and error handling; set up database connection

---
+++ server.js
@@ -0,0 +1,65 @@
+import dotenv from "dotenv";
+import connectDB from "./src/common/db/connection.js";
+import express from "express";
+import cors from "cors";
+// import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import logger from "./src/common/utils/logger.js";
+import { errorHandler } from "./src/middlewares/errorMiddlewares.js";
+
+dotenv.config({ path: "./.env" });
+const app = express();
+const port = process.env.PORT || 3000;
+const isProd = process.env.NODE_ENV === "production";
+
+// HTTP logs (dev only)
+if (!isProd) {
+  app.use(
+    morgan(":method :url :status :res[content-length] - :response-time ms", {
+      stream: {
+        write: (message) => logger.http(message.trim()),
+      },
+    })
+  );
+}
+
+const allowedOrigins = process.env.CORS_ORIGIN?.split(",") || [];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like curl or Postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        console.warn("❌ Blocked by CORS:", origin);
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+// app.use(cookieParser());
+
+// routers here
+app.get("/api/v1/health", (req, res) => res.status(200).send("ok"));
+
+app.use(errorHandler);
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error => ", error);
+      throw error;
+    });
+    app.listen(port, () => {
+      console.log(`Server is running on port => ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("DB connection failed => ", error);
+  });
---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const DB_NAME = "IEHE";
+
+// export const cookieOptions = {
+//   httpOnly: true,
+//   secure: process.env.NODE_ENV === "production",
+//   sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+// };
+
+export const adminGmail = "jiveemourya@gmail.com";
